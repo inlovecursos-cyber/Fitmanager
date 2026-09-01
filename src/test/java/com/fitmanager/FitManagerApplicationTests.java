e9f4be05fe4405f645b6bf341261e37583807ffa
inlovecursos-cyber <inlovecursos@gmail.com>
2026-09-01 11:33:03 +0100

    Cria estrutura inicial do FitManager

---
+++ src/test/java/com/fitmanager/FitManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
